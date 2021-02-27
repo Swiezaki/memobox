45884ffdf8c34424b5811e9174d38f2e5af667fd
Marcin Jastrzebski <marcin.jastrzebski@outlook.com>
2021-02-27 10:24:47 +0100

    init commit

---
+++ src/main/java/io/github/wojtekmarcin/memobox/MemoboxApplication.java
@@ -0,0 +1,13 @@
+package io.github.wojtekmarcin.memobox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/wojtekmarcin/memobox/MemoboxApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wojtekmarcin.memobox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
