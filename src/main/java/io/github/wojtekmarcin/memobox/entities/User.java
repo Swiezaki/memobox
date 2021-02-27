382926b35c757cc74448199c8b355ad5d3529779
WojtekMalka <malka_wojciech@wp.pl>
2021-02-27 12:13:00 +0100

    User entity modified

--- src/main/java/io/github/wojtekmarcin/memobox/entities/User.java
+++ src/main/java/io/github/wojtekmarcin/memobox/entities/User.java
@@ -1,4 +1,49 @@
 package io.github.wojtekmarcin.memobox.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long user_id;
+    private String login;
+    private String password;
+    private long memoBox_id;
+
+    public User() {
+    }
+
+    public long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getMemoBox_id() {
+        return memoBox_id;
+    }
+
+    public void setMemoBox_id(long memoBox_id) {
+        this.memoBox_id = memoBox_id;
+    }
 }
