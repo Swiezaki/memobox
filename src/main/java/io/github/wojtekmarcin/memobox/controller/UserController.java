f8622cef5e278d496a34e5c830c4093b73d2166d
WojtekMalka <malka_wojciech@wp.pl>
2021-03-07 13:06:20 +0100

    User repostiory added

--- src/main/java/io/github/wojtekmarcin/memobox/controller/UserController.java
+++ src/main/java/io/github/wojtekmarcin/memobox/controller/UserController.java
@@ -1,4 +1,7 @@
 package io.github.wojtekmarcin.memobox.controller;
 
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+
+@RepositoryRestController
 public class UserController {
 }
--- src/main/java/io/github/wojtekmarcin/memobox/repository/UserRepository.java
+++ src/main/java/io/github/wojtekmarcin/memobox/repository/UserRepository.java
@@ -2,8 +2,21 @@ package io.github.wojtekmarcin.memobox.repository;
 
 import io.github.wojtekmarcin.memobox.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Override
+    @RestResource(exported = false)
+    void delete(User user);
+
+    @RestResource(path = "deleteUserId", rel = "deleteUserId")
+    void deleteById(@Param("id") Long userId);
+
+    @RestResource(path = "user", rel = "user")
+    List<User> findUserByUser_id(@Param("id") Long userId);
 }
