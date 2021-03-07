c300b675cbab44c7bc113155a45aea7c1422e2d5
WojtekMalka <malka_wojciech@wp.pl>
2021-03-07 13:47:08 +0100

    UserController and UserRepo changes

--- src/main/java/io/github/wojtekmarcin/memobox/repository/UserRepository.java
+++ src/main/java/io/github/wojtekmarcin/memobox/repository/UserRepository.java
@@ -3,20 +3,13 @@ package io.github.wojtekmarcin.memobox.repository;
 import io.github.wojtekmarcin.memobox.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@RepositoryRestResource
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Override
-    @RestResource(exported = false)
-    void delete(User user);
-
-    @RestResource(path = "deleteUserId", rel = "deleteUserId")
-    void deleteById(@Param("id") Long userId);
-
     @RestResource(path = "user", rel = "user")
     List<User> findUserByUser_id(@Param("id") Long userId);
 }
