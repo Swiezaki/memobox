489bc89be372ade081c436d8378cb9ea9ea095c9
WojtekMalka <malka_wojciech@wp.pl>
2021-03-08 21:17:50 +0100

    User class variable user_id changed to userId
    I need to changed it because Hibernate has a problem
    with query creation

--- src/main/java/io/github/wojtekmarcin/memobox/repository/SqlUserRepository.java
+++ src/main/java/io/github/wojtekmarcin/memobox/repository/SqlUserRepository.java
@@ -2,10 +2,7 @@ package io.github.wojtekmarcin.memobox.repository;
 
 import io.github.wojtekmarcin.memobox.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 interface SqlUserRepository extends UserRepository, JpaRepository<User, Long> {
