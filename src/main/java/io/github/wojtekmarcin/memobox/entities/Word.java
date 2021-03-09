e09f3c95fb4e93253fca3cbcc752eb7f95920315
WojtekMalka <malka_wojciech@wp.pl>
2021-03-09 20:06:51 +0100

    Ready for harder changes on entities

--- src/main/java/io/github/wojtekmarcin/memobox/entities/Word.java
+++ src/main/java/io/github/wojtekmarcin/memobox/entities/Word.java
@@ -14,7 +14,7 @@ public class Word {
     private Integer wordLanguageId;
     private Integer wordTypeId;
 
-    @OneToOne(mappedBy = "wordId")
+    @OneToOne(mappedBy = "word")
     private WordsSet wordsSetWordId;
 
     public Word() {
