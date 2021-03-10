7cf69dff7f43f4d9e5c9782de8448f897d16bdfd
WojtekMalka <malka_wojciech@wp.pl>
2021-03-10 20:51:42 +0100

    WordsSet and Memobox repository added

--- src/main/java/io/github/wojtekmarcin/memobox/repository/MemoboxRepository.java
+++ src/main/java/io/github/wojtekmarcin/memobox/repository/MemoboxRepository.java
@@ -1,4 +1,15 @@
 package io.github.wojtekmarcin.memobox.repository;
 
+import io.github.wojtekmarcin.memobox.entities.MemoBox;
+
+import java.util.List;
+
 public interface MemoboxRepository {
+    List<MemoBox> findAll();
+
+    boolean existsById(Long id);
+
+    MemoBox findMemoBoxByMemoBoxId(Long id);
+
+    void deleteByMemoBoxId(Long id);
 }
--- src/main/java/io/github/wojtekmarcin/memobox/repository/WordsSetRepository.java
+++ src/main/java/io/github/wojtekmarcin/memobox/repository/WordsSetRepository.java
@@ -1,4 +1,16 @@
 package io.github.wojtekmarcin.memobox.repository;
 
+import io.github.wojtekmarcin.memobox.entities.MemoBox;
+import io.github.wojtekmarcin.memobox.entities.WordsSet;
+
+import java.util.List;
+
 public interface WordsSetRepository {
+    List<WordsSet> findAll();
+
+    boolean existsById(Long id);
+
+    MemoBox findMemoBoxByWordsSetId(Long id);
+
+    void deleteByWordsSetId(Long id);
 }
