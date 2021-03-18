c28d79b8c6fddcb3adac670a92123fe6e0591640
WojtekMalka <malka_wojciech@wp.pl>
2021-03-18 21:07:52 +0100

    UserView each loop added

--- src/main/java/io/github/wojtekmarcin/memobox/controller/UserViewController.java
+++ src/main/java/io/github/wojtekmarcin/memobox/controller/UserViewController.java
@@ -1,16 +1,20 @@
 package io.github.wojtekmarcin.memobox.controller;
 
+import io.github.wojtekmarcin.memobox.entities.MemoBox;
 import io.github.wojtekmarcin.memobox.entities.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
 
 @Controller
 @RequestMapping("/userView")
 public class UserViewController {
     @GetMapping
-    String showUserView(Model model){
+    String showUserView(Model model) {
         User userToEdit = new User();
         userToEdit.setLogin("test");
         model.addAttribute("users", userToEdit);
