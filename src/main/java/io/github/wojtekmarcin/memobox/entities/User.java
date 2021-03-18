9f82bd3ea654e5888bd934c392b24ef4524f28a8
WojtekMalka <malka_wojciech@wp.pl>
2021-03-18 20:18:18 +0100

    userView page controller

--- src/main/java/io/github/wojtekmarcin/memobox/entities/User.java
+++ src/main/java/io/github/wojtekmarcin/memobox/entities/User.java
@@ -39,7 +39,7 @@ public class User {
         this.userId = userId;
     }
 
-    public String getLogin() {
+    public String   getLogin() {
         return login;
     }
 
---
+++ src/main/java/io/github/wojtekmarcin/memobox/form/UserForm.java
@@ -0,0 +1,22 @@
+package io.github.wojtekmarcin.memobox.form;
+
+public class UserForm {
+    private String login;
+    private String password;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
