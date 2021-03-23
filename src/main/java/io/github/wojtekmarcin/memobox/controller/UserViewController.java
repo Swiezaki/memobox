1be32d2876319822e8235bdda55bc0d12a982b12
WojtekMalka <malka_wojciech@wp.pl>
2021-03-23 18:44:53 +0100

    Commit after long break

--- src/main/java/io/github/wojtekmarcin/memobox/controller/UserViewController.java
+++ src/main/java/io/github/wojtekmarcin/memobox/controller/UserViewController.java
@@ -1,23 +1,32 @@
 package io.github.wojtekmarcin.memobox.controller;
 
-import io.github.wojtekmarcin.memobox.entities.MemoBox;
 import io.github.wojtekmarcin.memobox.entities.User;
+import io.github.wojtekmarcin.memobox.repository.UserRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/userView")
 public class UserViewController {
+    private final UserRepository repository;
+
+    public UserViewController(UserRepository repository) {
+        this.repository = repository;
+    }
+
+//    @GetMapping
+//    String showUserView(Model model) {
+//        User userToEdit = new User();
+//        userToEdit.setLogin("test");
+//        model.addAttribute("user", userToEdit);
+//        return "userView";
+//    }
+
     @GetMapping
-    String showUserView(Model model) {
-        User userToEdit = new User();
-        userToEdit.setLogin("test");
-        model.addAttribute("users", userToEdit);
-        return "userView";
+    List<User> readAllUsers() {
+        return repository.findAll();
     }
 }
