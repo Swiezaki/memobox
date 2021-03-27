d6d71fb38776181b5746f75b12ef9293883f39e8
WojtekMalka <malka_wojciech@wp.pl>
2021-03-27 21:14:56 +0100

    WordView created corectlly

--- src/main/java/io/github/wojtekmarcin/memobox/controller/UserViewController.java
+++ src/main/java/io/github/wojtekmarcin/memobox/controller/UserViewController.java
@@ -1,10 +1,14 @@
 package io.github.wojtekmarcin.memobox.controller;
 
+import io.github.wojtekmarcin.memobox.entities.User;
 import io.github.wojtekmarcin.memobox.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/userView")
@@ -19,5 +23,10 @@ public class UserViewController {
     public String showUserView(Model model) {
         model.addAttribute("users", repository.findAll());
         return "userView";
+    }
+
+    @ModelAttribute("users")
+    public List<User> showUserView() {
+        return repository.findAll();
     }
 }
