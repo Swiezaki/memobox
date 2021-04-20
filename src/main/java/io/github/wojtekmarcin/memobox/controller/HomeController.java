647bd005e463bc33eab4da32d19e6ce2e948366a
WojtekMalka <malka_wojciech@wp.pl>
2021-04-20 20:11:34 +0200

    Logut button and controller

--- src/main/java/io/github/wojtekmarcin/memobox/controller/HomeController.java
+++ src/main/java/io/github/wojtekmarcin/memobox/controller/HomeController.java
@@ -1,5 +1,6 @@
 package io.github.wojtekmarcin.memobox.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +10,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
     @GetMapping("/")
-    String showHome(Model model) {
+    private String showHome(Model model) {
         return "home";
     }
 
     @GetMapping("/userPage")
-    String showUserPage(){
+    private String showUserPage(){
         return "/userPage";
     }
 
     @GetMapping("/adminPage")
-    String showAdminPage(){
+    private String showAdminPage(){
         return "/adminPage";
+    }
+
+    @GetMapping("/logout")
+    private void logout(){
+        SecurityContextHolder.getContext().setAuthentication(null);
     }
 }
