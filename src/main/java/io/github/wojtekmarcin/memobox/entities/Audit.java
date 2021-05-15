beeea8f53e4f2ed906a202dec6cfc0f7c746e7e1
WojtekMalka <malka_wojciech@wp.pl>
2021-05-15 09:47:48 +0200

    Entities has @Data annotation

--- src/main/java/io/github/wojtekmarcin/memobox/entities/Audit.java
+++ src/main/java/io/github/wojtekmarcin/memobox/entities/Audit.java
@@ -1,5 +1,8 @@
 package io.github.wojtekmarcin.memobox.entities;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Embeddable;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
@@ -7,6 +10,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Embeddable
+@Data
 public class Audit {
     private LocalDateTime createdOn;
     private LocalDateTime preMerge;
@@ -22,22 +26,6 @@ public class Audit {
     }
 
     public Audit() {
-    }
-
-    public LocalDateTime getCreatedOn() {
-        return createdOn;
-    }
-
-    public void setCreatedOn(LocalDateTime createdOn) {
-        this.createdOn = createdOn;
-    }
-
-    public LocalDateTime getPreMerge() {
-        return preMerge;
-    }
-
-    public void setPreMerge(LocalDateTime preMerge) {
-        this.preMerge = preMerge;
     }
 
     public String viewCreatedDate() {
--- src/main/java/io/github/wojtekmarcin/memobox/entities/Word.java
+++ src/main/java/io/github/wojtekmarcin/memobox/entities/Word.java
@@ -1,6 +1,7 @@
 package io.github.wojtekmarcin.memobox.entities;
 
 import io.github.wojtekmarcin.memobox.dictionary.LanguageEnum;
+import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
@@ -9,6 +10,7 @@ import javax.validation.constraints.*;
 import static io.github.wojtekmarcin.memobox.dictionary.ValidationMessage.*;
 
 @Entity
+@Data
 @Table(name = "Words")
 public class Word {
     public static final String PATTERN_ONLY_LETTERS = "[a-zA-Z]+";
@@ -39,77 +41,4 @@ public class Word {
 
     @Embedded
     private Audit audit = new Audit();
-
-    public Word() {
-    }
-
-    public long getWordId() {
-        return wordId;
-    }
-
-    public void setWordId(long wordId) {
-        this.wordId = wordId;
-    }
-
-    public String getWord() {
-        return word;
-    }
-
-    public void setWord(String word) {
-        this.word = word;
-    }
-
-    public LanguageEnum getWordLanguage() {
-        return wordLanguage;
-    }
-
-    public void setWordLanguage(LanguageEnum wordLanguage) {
-        this.wordLanguage = wordLanguage;
-    }
-
-    public String getWordTranslation() {
-        return wordTranslation;
-    }
-
-    public void setWordTranslation(String wordTranslation) {
-        this.wordTranslation = wordTranslation;
-    }
-
-    public LanguageEnum getTranslationLanguage() {
-        return translationLanguage;
-    }
-
-    public void setTranslationLanguage(LanguageEnum translationLanguage) {
-        this.translationLanguage = translationLanguage;
-    }
-
-    public WordsSet getWordSet() {
-        return wordSet;
-    }
-
-    public void setWordSet(WordsSet wordSet) {
-        this.wordSet = wordSet;
-    }
-
-    public Audit getAudit() {
-        return audit;
-    }
-
-    public void setAudit(Audit audit) {
-        this.audit = audit;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Word{");
-        sb.append("wordId=").append(wordId);
-        sb.append(", word='").append(word).append('\'');
-        sb.append(", wordLanguage=").append(wordLanguage);
-        sb.append(", wordTranslation='").append(wordTranslation).append('\'');
-        sb.append(", translationLanguage=").append(translationLanguage);
-        sb.append(", wordSet=").append(wordSet);
-        sb.append(", audit=").append(audit);
-        sb.append('}');
-        return sb.toString();
-    }
 }
